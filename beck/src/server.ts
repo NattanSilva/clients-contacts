827e3a54e855795cbf005828546a38348527a1a5
NattanSilva <natanxboy@gmail.com>
2023-05-22 15:32:40 -0300

    Add: server and data-source configs

---
+++ beck/src/server.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import app from './app';
+import AppDataSource from './data-source';
+
+const PORT = process.env.APP_PORT;
+
+(async () => {
+  await AppDataSource.initialize()
+    .then((res) => {
+      console.log('✅ Database connected');
+    })
+    .catch((err) => {
+      console.error('❌ Error during Data Source initialization -> ', err);
+    });
+
+  app.listen(4012, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
+})();
